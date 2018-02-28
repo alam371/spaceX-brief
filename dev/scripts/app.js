f6ad340e6b536a61ca0bc7dda82060d1b709793b
Joey Deol <gurjothdeol@gmail.com>
2018-02-28 13:02:17 -0500

    Created simple routes to all pages within application

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HomePage from './HomePage'; 
+import LaunchDetails from './LaunchDetails'; 
+import MultiLaunchMap from './MultiLaunchMap';
+import { Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Working</h2>
+        <Link to="/Home">Start</Link>
+      </div>
+    )
+  }
+}
 
 class App extends React.Component {
     render() {
       return (
-        <div>
-          Hello
-        </div>
+        <Router>
+          <div>
+            <Route path="/" exact component={LandingPage}/>
+            <Route path="/Home" exact component={HomePage} />
+            <Route path="/LaunchDetails" exact component={LaunchDetails} />
+            <Route path="/MultiLaunchMap" exact component={MultiLaunchMap} />
+          </div>
+        </Router>
       )
     }
 }
