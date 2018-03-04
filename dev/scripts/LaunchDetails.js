c220285fa768f89afea33c0850d02ed75ea6d21d
Angeline Lam <lam.angeline@gmail.com>
2018-03-04 11:09:30 -0500

    styling for launchDetails page sun1109

--- dev/scripts/LaunchDetails.js
+++ dev/scripts/LaunchDetails.js
@@ -26,42 +26,49 @@ class LaunchDetails extends React.Component {
     render() {
         console.log(this.state.missionInfo);
         return (
-            <div>
-                <Link to="/home">Home</Link>
-                <h2>LaunchDetails</h2>
-                {this.state.missionInfo.map((item) => {
-                    const videoID = item.links.video_link.replace('https://www.youtube.com/watch?v=','')
-                    return(
-                        <div key={item.flight_number}>
-                            <div>
-                                <div className="patch"><img src={item.links.mission_patch}/></div>
-                                <div className="flight-details-text">
-                                    <p>Flight: {item.flight_number} </p>
-                                    <p>Rocket Name: {item.rocket.rocket_name}</p>
-                                    <p>Launch Site: {item.launch_site.site_name_long}</p>
-                                    {item.details !== null && item.details !== undefined 
-                                    ? <p>Flight Highlight: {item.details}</p>
-                                    : null
+            <div className="launch_details__container">
+                <div className="wrapper">
+                    <Link to="/home">Home</Link>
+                    <h2>Launch Details</h2>
+                    {this.state.missionInfo.map((item) => {
+                        const videoID = item.links.video_link.replace('https://www.youtube.com/watch?v=','')
+                        return(
+                            <div key={item.flight_number}>
+                                <div className="launch_details__box clearfix">
+                                    <div className="patch">
+                                        <img src={item.links.mission_patch}/>
+                                    </div>
+
+                                    <div className="flight-details-text">
+                                        <p>Flight: {item.flight_number} </p>
+                                        <p>Rocket Name: {item.rocket.rocket_name}</p>
+                                        <p>Launch Site: {item.launch_site.site_name_long}</p>
+                                        {item.details !== null && item.details !== undefined 
+                                        ? <p>Flight Highlight: {item.details}</p>
+                                        : null
+                                        }
+                                    </div>
+                                </div>
+
+                                <iframe 
+                                src={`https://www.youtube.com/embed/${videoID}`} 
+                                frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
+                                </iframe>
+                                
+                                <div className="flight-details-links">
+                                    {item.links.article_link !== null && item.links.article_link !== undefined
+                                        ? <p>Article Link: <a href={item.links.article_link}>link</a></p>
+                                        : null
+                                    }
+                                    {item.links.reddit_launch !== null && item.links.reddit_launch !== undefined
+                                        ? <p>Reddit: <a href={item.links.reddit_launch} target="_blank">link</a></p>
+                                        : null
                                     }
                                 </div>
                             </div>
-                            <iframe width="600" height="480" 
-                            src={`https://www.youtube.com/embed/${videoID}`} 
-                            frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
-                            </iframe>
-                            <div className="flight-details-links">
-                                {item.links.article_link !== null && item.links.article_link !== undefined
-                                    ? <p>Article Link: <a href={item.links.article_link}>link</a></p>
-                                    : null
-                                }
-                                {item.links.reddit_launch !== null && item.links.reddit_launch !== undefined
-                                    ? <p>Reddit: <a href={item.links.reddit_launch} target="_blank">link</a></p>
-                                    : null
-                                }
-                            </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
         )
     }
