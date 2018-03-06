465f93805f9d2f103cb2390f40789c2b5eb35f56
Quinn Anderson <quinnrhys89@gmail.com>
2018-03-06 12:51:28 -0500

    Launch Details Finalized

--- dev/scripts/LaunchDetails.js
+++ dev/scripts/LaunchDetails.js
@@ -24,48 +24,68 @@ class LaunchDetails extends React.Component {
     render() {
         return (
             <div className="launch_details__container">
-                <div className="wrapper">
-                    <Link to="/home">Home</Link>
+                {/* <div className="wrapper"> */}
+                    
                     <h2>Launch Details</h2>
                     {this.state.missionInfo.map((item) => {
                         const videoID = item.links.video_link.replace('https://www.youtube.com/watch?v=','')
                         return(
                             <div key={item.flight_number}>
                                 <div className="launch_details__box clearfix">
-                                    <div className="patch">
-                                        <img src={item.links.mission_patch}/>
-                                    </div>
+                                    <div className="panel">
+                                        <div className="parallax">
 
-                                    <div className="flight-details-text">
-                                        <p>Flight: {item.flight_number} </p>
-                                        <p>Rocket Name: {item.rocket.rocket_name}</p>
-                                        <p>Launch Site: {item.launch_site.site_name_long}</p>
-                                        {item.details !== null && item.details !== undefined 
-                                        ? <p>Flight Highlight: {item.details}</p>
-                                        : null
-                                        }
-                                    </div>
-                                </div>
+                                            {/* Rocket Patch */}
+                                            <div className="patch">
+                                                <img src={item.links.mission_patch}/>
+                                            </div>
 
-                                <iframe 
-                                src={`https://www.youtube.com/embed/${videoID}`} 
-                                frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
-                                </iframe>
-                                
-                                <div className="flight-details-links">
-                                    {item.links.article_link !== null && item.links.article_link !== undefined
-                                        ? <p>Article Link: <a href={item.links.article_link}>link</a></p>
-                                        : null
-                                    }
-                                    {item.links.reddit_launch !== null && item.links.reddit_launch !== undefined
-                                        ? <p>Reddit: <a href={item.links.reddit_launch} target="_blank">link</a></p>
-                                        : null
-                                    }
+                                            <div className="launch-info-container">
+                                                {/* Flight Stats */}
+                                                <div className="flight-details-text">
+                                                    <h4 className="flight-stats">Flight Stats</h4>
+                                                    <p>Flight Number : {item.flight_number} </p>
+                                                    <p>Rocket Name : {item.rocket.rocket_name}</p>
+                                                    <p>Launch Site : {item.launch_site.site_name_long}</p>
+                                                    {item.details !== null && item.details !== undefined 
+                                                    ? <p>Flight Highlight : {item.details}</p>
+                                                    : null
+                                                    }
+                                                </div>
+                                                
+                                                {/* Links to Articles */}
+                                                <div className="flight-details-links">
+                                                    <h4 className="about-this-flight">More About This Flight</h4>
+                                                    {item.links.article_link !== null && item.links.article_link !== undefined
+                                                        ? <p><a href={item.links.article_link}>Article Link</a></p>
+                                                        : null
+                                                    }
+                                                    {item.links.reddit_launch !== null && item.links.reddit_launch !== undefined
+                                                        ? <p><a href={item.links.reddit_launch} target="_blank">Reddit Article</a></p>
+                                                        : null
+                                                    }
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="video-container">
+                                        <div className="panel">
+                                            <div className="parallax">
+                                                <iframe 
+                                                src={`https://www.youtube.com/embed/${videoID}`} 
+                                                frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
+                                                </iframe>
+                                            </div>    
+                                        </div>
+                                    </div>
+                                <Link to="/home"><i class="far fa-arrow-alt-circle-left"></i></Link>
                                 </div>
                             </div>
                         )
                     })}
-                </div>
+                {/* </div> */}
             </div>
         )
     }
