c08d57ae79aef5d5df570af47babc16c1b4df2ac
Angeline Lam <lam.angeline@gmail.com>
2018-03-06 20:45:34 -0500

    responsive styling

--- dev/scripts/LaunchDetails.js
+++ dev/scripts/LaunchDetails.js
@@ -32,53 +32,54 @@ class LaunchDetails extends React.Component {
                         return(
                             <div key={item.flight_number}>
                                 <div className="launch_details__box clearfix">
-                                    <div className="panel">
-                                        <div className="parallax">
+                                    <div className="panel panel-override clearfix">
+                                            <div className="parallax parallaxOverride">
 
-                                            {/* Rocket Patch */}
-                                            <div className="patch">
-                                                <img src={item.links.mission_patch}/>
-                                            </div>
+                                                {/* Rocket Patch */}
+                                                <div className="patch">
+                                                    <img src={item.links.mission_patch}/>
+                                                </div>
 
-                                            <div className="launch-info-container">
-                                                {/* Flight Stats */}
-                                                <div className="flight-details-text">
-                                                    <h4 className="flight-stats">Flight Stats</h4>
-                                                    <p>Flight Number : {item.flight_number} </p>
-                                                    <p>Rocket Name : {item.rocket.rocket_name}</p>
-                                                    <p>Launch Site : {item.launch_site.site_name_long}</p>
-                                                    {item.details !== null && item.details !== undefined 
-                                                    ? <p>Flight Highlight : {item.details}</p>
-                                                    : null
-                                                    }
+                                                <div className="launch-info-container">
+                                                    {/* Flight Stats */}
+                                                    <div className="flight-details-text">
+                                                        <h4 className="flight-stats">Flight Stats</h4>
+                                                        <p>Flight Number : {item.flight_number} </p>
+                                                        <p>Rocket Name : {item.rocket.rocket_name}</p>
+                                                        <p>Launch Site : {item.launch_site.site_name_long}</p>
+                                                        {item.details !== null && item.details !== undefined 
+                                                        ? <p>Flight Highlight : {item.details}</p>
+                                                        : null
+                                                        }
+                                                    </div>
+                                                    
+                                                    {/* Links to Articles */}
+                                                    <div className="flight-details-links">
+                                                        <h4 className="about-this-flight">More About This Flight</h4>
+                                                        {item.links.article_link !== null && item.links.article_link !== undefined
+                                                            ? <p><a href={item.links.article_link}>Article Link</a></p>
+                                                            : null
+                                                        }
+                                                        {item.links.reddit_launch !== null && item.links.reddit_launch !== undefined
+                                                            ? <p><a href={item.links.reddit_launch} target="_blank">Reddit Article</a></p>
+                                                            : null
+                                                        }
+                                                    </div>
                                                 </div>
                                                 
-                                                {/* Links to Articles */}
-                                                <div className="flight-details-links">
-                                                    <h4 className="about-this-flight">More About This Flight</h4>
-                                                    {item.links.article_link !== null && item.links.article_link !== undefined
-                                                        ? <p><a href={item.links.article_link}>Article Link</a></p>
-                                                        : null
-                                                    }
-                                                    {item.links.reddit_launch !== null && item.links.reddit_launch !== undefined
-                                                        ? <p><a href={item.links.reddit_launch} target="_blank">Reddit Article</a></p>
-                                                        : null
-                                                    }
-                                                </div>
                                             </div>
-                                            
                                         </div>
                                     </div>
                                     
                                     <div className="video-container">
-                                        <div className="panel">
-                                            <div className="parallax">
+                                        <div className="panel panel-override">
+                                            <div className="parallax parallaxOverride">
                                                 <iframe 
                                                 src={`https://www.youtube.com/embed/${videoID}`} 
                                                 frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen>
                                                 </iframe>
                                             </div>    
-                                        </div>
+                                      
                                     </div>
                                 <Link to="/home"><i class="far fa-arrow-alt-circle-left"></i></Link>
                                 </div>
